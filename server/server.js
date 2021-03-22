2c7826a379362c707f5ea76d82ea01376cce1505
Sam Pearson <sam.ed.pearson@gmail.com>
2021-03-22 18:51:49 -0500

    Removing bodyparser, using express instead

--- server/server.js
+++ server/server.js
@@ -1,10 +1,7 @@
 //Using express as the web server
 const express = require('express')
-//Using fetch for API calls
-const fetch = require('node-fetch')
-//Using bodyparser to handle JS sent in the request
-const bodyParser = require('body-parser')
-//Creating a variable to cal express from
+
+//Creating a variable to call express from
 const app = express()
 //Lodash provides various utitility functions such as searching arrays
 const _ = require('lodash')
@@ -13,9 +10,9 @@ const _ = require('lodash')
 // restrict what ports we can use, use port 3000.
 const PORT = process.env.port || 3000
 
-//Use bodyparser to handle json posted to the server
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+//Use express (instead of bodyparser) to handle json posted to the server
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
 //A sandbox for creating new routes or just goofin around
 const debug_endpoints = require("./routes/debug_endpoints")
